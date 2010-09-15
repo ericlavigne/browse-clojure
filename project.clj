8a0c9c6ad6158bfd0fa432270d6a607afd800235
Eric Lavigne <lavigne.eric@gmail.com>
2010-09-14 22:20:59 -0400

    Added Enlive library and used it to fetch repository names from Clojars. Increased Clojure version from 1.1 to 1.2.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject browse-clojure "0.1"
   :description "An easy-to-browse directory of Clojure libraries"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 [enlive "1.0.0-SNAPSHOT"]])
--- src/browse_clojure/core.clj
+++ src/browse_clojure/core.clj
@@ -1,1 +1,13 @@
-(ns browse-clojure.core)
+(ns browse-clojure.core
+  (:require [net.cgrand.enlive-html :as enlive-html]))
+
+(defn fetch-clojars-repo-names []
+  (let [html-tree (enlive-html/html-resource 
+		   (java.net.URL. "http://clojars.org/repo/"))
+	link-texts (map enlive-html/text
+			(enlive-html/select html-tree [:td :a]))
+	repo-texts (drop 3 
+			 (take-while #(not (= % "all-jars.clj"))
+				     link-texts))]
+    repo-texts))
+
