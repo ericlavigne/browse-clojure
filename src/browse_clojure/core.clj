6752423bd2e6119633f48fed7bb7af38d6812bcc
Eric Lavigne <lavigne.eric@gmail.com>
2011-01-25 00:06:47 -0500

    Building 2-mode network with programmers and Clojure projects. The first three generations have 1 (clojure), 50, and 168 programmers. That third generation takes about 20 minutes to generate, compared to just half a minute for the second generation. The wrong versions of projects are selected, because I let zipmap select which version from a particular generation will be considered instead of explicitely merging.

--- src/browse_clojure/core.clj
+++ src/browse_clojure/core.clj
@@ -1,7 +1,8 @@
 (ns browse-clojure.core
   (:require [net.cgrand.enlive-html :as enlive-html]
             [clj-http.client :as http]
-            [clojure.contrib.json :as json]))
+            [clojure.contrib.json :as json]
+            clojure.set))
 
 (def fetching-throttle (agent (org.joda.time.DateTime.)))
 
@@ -70,3 +71,70 @@
         lein-files (map #(github-file user project (% file-listing))
                         lein-filenames)]
     lein-files))
+
+(def initial-network
+     {:programmers #{"clojure"}
+      :programmers-new-gen #{"clojure"}
+      :programmers-by-gen [#{"clojure"}]
+      :projects {}})
+                                        ; {project-name {:owner
+					;                :total-contributions
+					;                :contributions
+					;                :name
+					;                :all-owners}
+
+(defn merge-projects [proj1 proj2]
+  (let [main-proj (if (< (:total-contributions proj1)
+                         (:total-contributions proj2))
+                    proj2
+                    proj1)]
+    {:owner (:owner main-proj)
+     :total-contributions (:total-contributions main-proj)
+     :contributions (:contributions main-proj)
+     :name (:name main-proj)
+     :all-owners (clojure.set/union (:all-owners proj1)
+                                    (:all-owners proj2))}))
+
+(defn advance-generation [old-generation]
+  (let [new-gen-projects (distinct (mapcat (fn [user]
+                                             (map (fn [proj]
+                                                    {:name (:name proj)
+                                                     :owner (:owner proj)})
+                                                  (user-projects user)))
+                                           (:programmers-new-gen old-generation)))
+        discovered-projects (remove (fn [proj] (and (contains? (:projects old-generation)
+                                                               (:name proj))
+                                                    (contains? (:all-owners ((:projects old-generation)
+                                                                               (:name proj)))
+                                                               (:owner proj))))
+                                    new-gen-projects)
+        discovered-clojure-projects (filter (fn [proj] (project-includes-clojure (:owner proj) (:name proj)))
+                                            discovered-projects)
+        projects-with-contributions (map (fn [proj]
+                                           (let [owner (:owner proj)
+                                                 proj-name (:name proj)
+                                                 contributions (project-contributors owner proj-name)
+                                                 contributors (map first contributions)
+                                                 contribution-amounts (map second contributions)]
+                                             {:owner owner
+                                              :name proj-name
+                                              :all-owners #{owner}
+                                              :total-contributions (reduce + 0 contribution-amounts)
+                                              :contributions (zipmap contributors
+                                                                     contribution-amounts)}))
+                                         discovered-clojure-projects)
+        projects-with-enough-contributors (filter (fn [proj] (< 1 (count (:contributions proj))))
+                                                  projects-with-contributions)
+        next-gen-programmers (clojure.set/difference (set (mapcat (fn [proj] (keys (:contributions proj)))
+                                                                  projects-with-enough-contributors))
+                                                     (:programmers old-generation))
+        new-project-map (merge-with merge-projects
+                                    (:projects old-generation)
+                                    (zipmap (map :name projects-with-enough-contributors)
+                                            projects-with-enough-contributors))]
+    {:programmers (clojure.set/union (:programmers old-generation)
+                                     next-gen-programmers)
+     :programmers-new-gen next-gen-programmers
+     :programmers-by-gen (conj (:programmers-by-gen old-generation)
+                               next-gen-programmers)
+     :projects new-project-map}))
